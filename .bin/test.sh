1b42c1605fdae71a85923a0a5676a5d91fc84c94
Milen Radkov <milenradkov@me.com>
2018-12-13 13:16:53 +0200

    Update test.sh

--- .bin/test.sh
+++ .bin/test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ganache-cli --gasLimit <yourLimit> 2> /dev/null 1> /dev/null &
+ganache-cli 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
 cd smart-contract
