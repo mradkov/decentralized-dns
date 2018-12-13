16f0dda776545692d9a9afa3121a4844a3fc3e5a
Milen Radkov <milenradkov@me.com>
2018-12-13 13:49:10 +0200

    add coverage

--- bin/test.sh
+++ bin/test.sh
@@ -9,4 +9,6 @@ cd smart-contract
 truffle compile
 truffle migrate --reset --network development
 truffle test
+sleep 5
+npm run coverage && cat coverage/lcov.info | coveralls
 kill -9 $(lsof -t -i:8545)
