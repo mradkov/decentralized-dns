5024e0833973d914d1374054811812dc8f9e4dbf
Milen Radkov <milenradkov@me.com>
2018-12-14 19:05:29 +0200

    reorder test shell

--- bin/test.sh
+++ bin/test.sh
@@ -6,6 +6,7 @@ ganache-cli 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
 cd smart-contract
+npm install chai chai-bignumber ethereumjs-util ethjs-abi web3-utils --save
 truffle compile
 truffle migrate --reset --network development
 truffle test
--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 npm install
-npm install -g ganache-cli truffle coveralls solidity-coverage chai chai-bignumber ethereumjs-util ethjs-abi web3-utils
+npm install -g ganache-cli truffle coveralls solidity-coverage
