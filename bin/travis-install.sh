cf923c72aa16a8a6fdf46291ea2d5e8d2ebde93f
Milen Radkov <milenradkov@me.com>
2018-12-14 18:59:42 +0200

    added tests for safemath and fixed dependeces for travis

--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 npm install
-npm install -g ganache-cli truffle coveralls solidity-coverage
+npm install -g ganache-cli truffle coveralls solidity-coverage chai chai-bignumber ethereumjs-util ethjs-abi web3-utils
